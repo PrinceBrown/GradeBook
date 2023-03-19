e7078d8c05a1ab7c94c9086d1633d0f8a68cd956
Prince Brown <codingproprince@gmail.com>
2023-03-19 16:34:18 -0400

    all foldesrs & files

---
+++ GradeBook.Tests/StudentTests.cs
@@ -0,0 +1,23 @@
+﻿
+using GradeBook;
+
+namespace GradeBook.Tests;
+
+public class StudentTests
+{
+    [Fact]
+    public void Test1()
+    {
+        //arrange
+        Student student = new Student("Ashley", 29);
+        //student.Age = 13;
+        bool expected = false;
+
+        //act
+         bool actual = student.CheckIsAChild();
+
+        //assert
+        Assert.Equal(expected, actual);
+
+    }
+}
